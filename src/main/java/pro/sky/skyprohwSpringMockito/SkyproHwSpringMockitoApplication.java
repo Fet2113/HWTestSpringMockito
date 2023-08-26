27a3a7ceb0de50df984b675f8c2306cc06525bc7
Fet2113 <super.gjoll@yandex.ru>
2023-08-26 17:04:21 +0300

    ДЗ по покрытию тестами Employees(Mockito).

---
+++ src/main/java/pro/sky/skyprohwSpringMockito/SkyproHwSpringMockitoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprohwSpringMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproHwSpringMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproHwSpringMockitoApplication.class, args);
+	}
+
+}
